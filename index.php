8f21992becf7c8683b575e211100c84ccb569824
Ronan Quirke <ronan.quirke@gmail.com>
2013-06-21 22:36:03 +1200

    public sample in index

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
         <ol>
             <li>A sample script for Private API applications <a href="private.php">private.php</a>
             </li>
-            <li>A sample script for Partner API applications <a href="partner.php">partner.php</a>
+            <li>A sample script for Public API applications <a href="public.php">public.php</a>
+            </li>
+              <li>A sample script for Partner API applications <a href="partner.php">partner.php</a>
             </li>
         </ol>
       
