a828b8e2b12681cfd066b12402b185da87287906
Ronan Quirke <ronan.quirke@xero.com>
2015-02-03 15:07:06 +1300

    Fix for php_self ssl issue #18

--- lib/XeroOAuth.php
+++ lib/XeroOAuth.php
@@ -562,27 +562,34 @@ class XeroOAuth {
 	 *        	whether to drop the querystring or not. Default true
 	 * @return string the current URL
 	 */
-	public static function php_self($dropqs = true) {
-		$url = sprintf ( '%s://%s%s', empty ( $_SERVER ['HTTPS'] ) ? (@$_SERVER ['SERVER_PORT'] == '443' ? 'https' : 'http') : 'http', $_SERVER ['SERVER_NAME'], $_SERVER ['REQUEST_URI'] );
-		
-		$parts = parse_url ( $url );
-		
-		$port = $_SERVER ['SERVER_PORT'];
-		$scheme = $parts ['scheme'];
-		$host = $parts ['host'];
-		$path = @$parts ['path'];
-		$qs = @$parts ['query'];
-		
-		$port or $port = ($scheme == 'https') ? '443' : '80';
-		
-		if (($scheme == 'https' && $port != '443') || ($scheme == 'http' && $port != '80')) {
-			$host = "$host:$port";
+	public static function php_self($dropqs = true)
+	{
+	$protocol = 'http';
+	if (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on')
+		{
+		$protocol = 'https';
 		}
-		$url = "$scheme://$host$path";
-		if (! $dropqs)
-			return "{$url}?{$qs}";
-		else
-			return $url;
+	elseif (isset($_SERVER['SERVER_PORT']) && ($_SERVER['SERVER_PORT'] == '443'))
+		{
+		$protocol = 'https';
+		}
+
+	$url = sprintf('%s://%s%s', $protocol, $_SERVER['SERVER_NAME'], $_SERVER['REQUEST_URI']);
+	$parts = parse_url($url);
+	$port = $_SERVER['SERVER_PORT'];
+	$scheme = $parts['scheme'];
+	$host = $parts['host'];
+	$path = @$parts['path'];
+	$qs = @$parts['query'];
+	$port or $port = ($scheme == 'https') ? '443' : '80';
+	if (($scheme == 'https' && $port != '443') || ($scheme == 'http' && $port != '80'))
+		{
+		$host = "$host:$port";
+		}
+
+	$url = "$scheme://$host$path";
+	if (!$dropqs) return "{$url}?{$qs}";
+	  else return $url;
 	}
 	
 	/*
